102ae9b52ce6694b4dfb0048c40eebd48f60833a
GabrielChuchuca <gavriellalx@hotmail.com>
2021-05-11 10:04:55 -0500

    Subida Proyecto

---
+++ src/ec/edu/ups/modelo/prueba.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ec.edu.ups.modelo;
+
+/**
+ * @author Gabriel Leonardo Chu
+ *
+ */
+public class prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("AGENDA");
+
+	}
+
+}
